2cc2ef438b47afd0a1f0444e626669c7b5c9ec87
MSaad001 <sm7226336@gmail.com>
2024-05-21 00:43:40 +0500

    ngIF ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,37 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   oddNumbers: number[] = [];
+//   evenNumbers: number[] = [];
+
+//   onIntervalFired(firedNumber: number) {
+//     if (firedNumber % 2 === 0) {
+//       this.evenNumbers.push(firedNumber);
+//     }
+//     else {
+//       this.oddNumbers.push(firedNumber);
+//     }
+
+//   }
+  
+// }
+
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  oddNumbers: number[] = [];
-  evenNumbers: number[] = [];
-
-  onIntervalFired(firedNumber: number) {
-    if (firedNumber % 2 === 0) {
-      this.evenNumbers.push(firedNumber);
-    }
-    else {
-      this.oddNumbers.push(firedNumber);
-    }
-
-  }
-  
+  // numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,47 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppComponent } from './app.component';
+// import { ServerComponent } from './server/server.component';
+// import { ServersComponent } from './servers/servers.component';
+// import { FormsModule } from '@angular/forms';
+// import { GameControlComponent } from './game-control/game-control.component';
+// import { OddComponent } from './odd/odd.component';
+// import { EvenComponent } from './even/even.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     ServerComponent,
+//     ServersComponent,
+//     GameControlComponent,
+//     OddComponent,
+//     EvenComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     FormsModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { FormsModule } from '@angular/forms';
-import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './odd/odd.component';
-import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    GameControlComponent,
-    OddComponent,
-    EvenComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

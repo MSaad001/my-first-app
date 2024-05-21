d208dc873cac2d45cd2da3002fd2cfa72cbf83f8
MSaad001 <sm7226336@gmail.com>
2024-05-21 19:53:16 +0500

    custom attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,10 +34,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
